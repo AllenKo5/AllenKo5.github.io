c90b427860c945f9ea82e12ca441eeb2436a96f6
AllenKo5 <69921088+AllenKo5@users.noreply.github.com>
2024-10-16 19:33:56 -0400

    created archives

---
+++ projects/archives/src/js/main.js
@@ -0,0 +1,24 @@
+const deckInput = document.querySelector('#deck-input');
+const deckOutput = document.querySelector('#deck-output');
+const button = document.querySelector('#button');
+
+const init = () => {
+    button.addEventListener('click', () => {
+        let output='';
+        const lines = deckInput.value.split('\n');
+        for (let i = 0; i < lines.length; i++){
+            if (lines[i] == '[Sideboard]') {
+                output += 'Sideboard\n';
+            }
+            if (lines[i].indexOf('+') > -1) {
+                output += `${lines[i].substring(0, lines[i].indexOf('+'))}\n`;
+            }
+            else if (lines[i].indexOf('|') > -1) {
+                output += `${lines[i].substring(0, lines[i].indexOf('|'))}\n`;
+            }
+        }
+        deckOutput.innerHTML = output;
+    });
+};
+
+init();
